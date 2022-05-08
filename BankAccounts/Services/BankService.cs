0c76c51689063adbe2b1177910ea09f4a9c1536f
bl48465 <67877209+bl48465@users.noreply.github.com>
2022-05-09 00:24:26 +0200

    SMTH

---
+++ BankAccounts/Services/BankService.cs
@@ -0,0 +1,34 @@
+﻿using BankAccounts.Interfaces;
+using BankAccounts.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace BankAccounts.Services;
+
+public class BankService : IBankService
+{
+
+    private readonly UserManager<User> _userManager;
+
+    public BankService(UserManager<User> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public Task<decimal> CurrentBalance(string userId)
+    {
+        var user = _userManager.Users.FirstOrDefault(x => x.Id == userId);
+        //if (user != null) { return user.Balance; }
+        //else { return null }
+
+    }
+
+    public Task<Transaction> Deposit(decimal amount)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Transaction> Withdraw(decimal amount)
+    {
+        throw new NotImplementedException();
+    }
+}
